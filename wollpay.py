ec574ea0173d7d5bb94771493cfe886f77828120
WorkSpace Python <78704527+oglinked@users.noreply.github.com>
2024-01-23 17:06:11 +0200

    Update wollpay.py

--- wollpay.py
+++ wollpay.py
@@ -1,5 +1,31 @@
 """wollpay.py (version 00.07 without balance.py) is programmes' Hub.
 To run wollpay.py you should run startpay.py program.
+
+Project wollpay.py is a CLI version of programmes' Hub for bank transactions' 
+small startup, written fully with Python programming language.
+
+WARNING: 
+If you going to run exactly wollpay.py in python environment, you should run 
+startpay.py program. It's importent tricky moment to avoid 
+"ImportError:(most likely due to a circular import)".
+If you gonna run wollpay.exe instead, than simply run it!
+
+The functional inside wollpay.py is:
+1. To make a receipt for the new transaction;
+2. To append clients base with the new client;
+3. To append banks accounts data of the WollPay clients;
+4. To edit CSV files' database contents.
+
+After programm starts you choose the item from menu "What to do" and 
+run one of programmes:
+1. receipt.py
+2. clientsbase.py
+3. bankaccount.py
+4. csvedit.py
+You can always return to the main menu or EXIT the program.
+
+Have a good day!
+Good luck!
 """
 
 import os
